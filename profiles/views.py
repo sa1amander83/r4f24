771e403630f5277a7c16e873f1f7cc66cf255bd2
sa1amander83 <virom8@yandex.ru>
2024-06-05 10:05:18 +0300

    ебалово2

--- profiles/views.py
+++ profiles/views.py
@@ -29,7 +29,7 @@ class ProfileUser(LoginRequiredMixin, ListView, DataMixin):
 
     def get_object(self, queryset=None):
         return self.request.user
-
+#aaaaaaaaaaa
     # TODO здесь должно быть только отображение  из модели статистики а не расчеты
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
