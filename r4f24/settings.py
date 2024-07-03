1986652dc65f997d1f31ed135b171ca8c5868c09
DreemzDev <149058593+DreemzDev@users.noreply.github.com>
2024-07-03 14:19:50 +0300

    first commit

--- r4f24/settings.py
+++ r4f24/settings.py
@@ -220,14 +220,22 @@ WSGI_APPLICATION = 'r4f24.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': "django.db.backends.postgresql",
+#         'HOST': 'localhost',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PORT': '5432',
+#         'PASSWORD': '123'}
+# }
+
+# sqlite
 DATABASES = {
     'default': {
-        'ENGINE': "django.db.backends.postgresql",
-        'HOST': 'localhost',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PORT': '5432',
-        'PASSWORD': '123'}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
