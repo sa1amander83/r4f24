b0a0ae2821df494c40a2d762e9776665c133deb6
DreemzDev <149058593+DreemzDev@users.noreply.github.com>
2024-07-11 17:51:05 +0300

    11.06

---
+++ tailwind.config_old.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./templates/**/*.html",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: ["tailwindcss ,autoprefixer", require("flowbite/plugin")],
+};
