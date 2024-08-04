f733c1667e941d2d94220ef79409daa2a71f3f01
sa1amander83 <virom8@yandex.ru>
2024-08-04 18:37:32 +0300

    update

--- r4f24/settings.py
+++ r4f24/settings.py
@@ -288,8 +288,8 @@ LOGGING = {
 # CELERY_BROKER_URL = 'redis://redis:6379'
 # CELERY_RESULT_BACKEND = 'redis://redis:6379'
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_BROKER_URL = 'redis://127.0.0.1:63790/0'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:63790/0'
 # CELERY_RESULT_EXTENDED = True
 # CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 # CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers.DatabaseScheduler'
@@ -337,6 +337,8 @@ CACHES = {
 #         "TIMEOUT": 60 * 15,
 #     }
 # }
+
+
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
